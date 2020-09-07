4ce1a814bd251fb11644f210eaa92601a6b3a732
Chandler L <cbl0015@auburn.edu>
2020-09-07 17:20:01 -0400

    Creation of initial files and DB

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO burgers(name, isDevoured) VALUES ("Bourbon Bacon Cheese Burger", false);
+
+INSERT INTO burgers(name, isDevoured) VALUES ("Mushroom and Swiss Cheese Burger", true);
+
+INSERT INTO burgers(name, isDevoured) VALUES ("Black and Blue Cheese Burger", false);
---
+++ server.js
@@ -0,0 +1,7 @@
+// Install Dependencies
+const express = require('express');
+
+// Allow server to grab any port, but set 7000 as standard default
+const PORT = process.env.PORT || 7000;
+
+const app = express();
