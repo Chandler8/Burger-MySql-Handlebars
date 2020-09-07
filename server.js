1e38b17214304cb6d4b7c97d5f475c5197dad524
Chandler L <cbl0015@auburn.edu>
2020-09-07 19:14:49 -0400

    Map out server destinations

--- server.js
+++ server.js
@@ -5,3 +5,25 @@ const express = require('express');
 const PORT = process.env.PORT || 7000;
 
 const app = express();
+
+app.use(express.static('public'));
+
+// Parse the body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Bring in Handlebars
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Integrate routes
+const routes = require('./controllers/burgers_controller.js');
+
+app.use(routes);
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server listening on: http://localhost: ${PORT}`);
+});
