57119e06bc0665a126f53ee006edc2239643dcc3
Chandler L <cbl0015@auburn.edu>
2020-09-10 18:06:12 -0400

    Front End Work Completed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 
 // Allow server to grab any port, but set 7000 as standard default
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT ||7000;
 
 const app = express();
 
