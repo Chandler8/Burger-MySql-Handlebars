221c57f057946e46b7ba57e2ea7e6076a88a66c4
Chandler L <cbl0015@auburn.edu>
2020-09-12 00:37:02 -0400

    Updates of Server

--- config/connection.js
+++ config/connection.js
@@ -9,6 +9,13 @@ var connection = mysql.createConnection({
     database: 'burger_db'
   });
 
+  module.exports = {
+    HOST: "us-cdbr-east-02.cleardb.com",
+    USER: "b9485f7de3b34d",
+    PASSWORD: "c6d4edd2",
+    DB: "heroku_30d89f8a71a1d8a"
+  };
+
 connection.connect((err) => {
   if (err) {
     console.log(`Error connecting: ${err.stack}`);
