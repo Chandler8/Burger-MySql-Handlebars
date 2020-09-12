44f960b74ba213bd5363c44e00107fb762e8ebfd
Chandler L <cbl0015@auburn.edu>
2020-09-11 23:32:44 -0400

    Attempting to connect DB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Connect to MySQL
 const mysql = require('mysql');
 
-var connection = mysql.createConnection({
+let connection
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
     password: 'Auburn',
     database: 'burger_db'
-  });
+  })
+};
 
 connection.connect((err) => {
   if (err) {
