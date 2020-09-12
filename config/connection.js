96e1a27d2b47dfa2b4a4a96257a8390ae792db18
Chandler L <cbl0015@auburn.edu>
2020-09-11 23:39:19 -0400

    Back to old method

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,13 @@
 // Connect to MySQL
 const mysql = require('mysql');
 
-let connection
-
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-connection = mysql.createConnection({
+var connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
     password: 'Auburn',
     database: 'burger_db'
-  })
-};
+  });
 
 connection.connect((err) => {
   if (err) {
